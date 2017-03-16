f1d1f8033f5dad0742081d37f53a28262f5e7e0e
Pedro Alarcon <PedroAlarcon@Pedros-MacBook-Pro.local>
2017-03-16 17:39:16 +0100

    cosa rara

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<h1> Hola ñoños </h1>
+	<hr>
+	<script>
+		document.write("¿bien o no?")
+	</script>
+	<hr>
+
+	<?php
+	echo "adiós";  
+	?>
+
+</body>
+</html>
